28b6639f158ba64181e2e6e0541edc36a051e0f8
Dimitar Mitev <dnmitev@gmail.com>
2018-05-28 19:45:44 +0300

    mvp of vehicle

--- test.js
+++ test.js
@@ -1,6 +1,18 @@
 (function() {
     "use strict";
 
-    var vehicle =  new Vehicle();
-    console.log(vehicle);
+    var vehicle1 =  new Vehicle('VW', 'up', 'CA1111CA');
+    vehicle1.drive(123);
+    console.log(vehicle1.miliage);
+
+    var vehicle2 =  new Vehicle('VW', 'up', 'CA2222CA');
+    vehicle2.drive(123);
+    console.log(vehicle2.miliage);
+
+    vehicle1.drive(124);
+    console.log(vehicle1.miliage);
+    debugger;
+    vehicle2.drive(556);
+    console.log(vehicle2.miliage);
+    
 }());
--- vehicle.js
+++ vehicle.js
@@ -1,9 +1,78 @@
-var Vehicle = (function() {
+var Vehicle = (function () {
     "use strict";
 
-    class Vehicle {
-        constructor() {
+    const MIN_MAKE_LENGTH = 2;
+    const MAX_MAKE_LENGTH = 25;
 
+    const MIN_MODEL_LENGTH = 2;
+    const MAX_MODEL_LENGTH = 25;
+
+    const LICENSE_NUM_LENGTH = 8;
+
+    class Vehicle {
+        constructor(make, model, licenseNum) {
+            this.make = make;
+            this.model = model;
+            this.licenseNum = licenseNum;
+            this._miliage = 0;
+        }
+
+        set make(value) {
+            _validateStringLength(
+                value, MIN_MAKE_LENGTH, MAX_MAKE_LENGTH, 'make');
+            _validateType(value, 'string');
+
+            this._make = value;
+        }
+
+        get make() {
+            return this._make;
+        }
+
+        set model(value) {
+            _validateStringLength(
+                value, MIN_MODEL_LENGTH, MAX_MODEL_LENGTH, 'model');
+            _validateType(value, 'string');
+
+            this._model = value;
+        }
+
+        get model() {
+            return this._model;
+        }
+
+        set licenseNum(value) {
+            _validateStringLength(
+                value, LICENSE_NUM_LENGTH, LICENSE_NUM_LENGTH, 'licenseNum');
+            _validateType(value, 'string');
+
+            this._licenseNum = value;
+        }
+
+        get licenseNum() {
+            return this._licenseNum;
+        }
+
+        get miliage() {
+            return this._miliage;
+        }
+
+        drive(distance) {
+            // TODO: Validate distance
+            this._miliage += distance;
+        }
+    }
+
+    function _validateStringLength(str, min, max, prop) {
+        if (str.length < min || str.length > max) {
+            throw new RangeError(
+                `Invalid range for vehicle's ${prop}: ${str} -> ${str.length}. Range - [${min}-${max}]`);
+        }
+    }
+
+    function _validateType(value, expectedType, prop) {
+        if (typeof (value) !== expectedType) {
+            throw new TypeError(`Invalid ${prop} type. Type was expected to be ${expectedType}`);
         }
     }
 
